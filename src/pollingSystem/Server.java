db9c8cf759950d04768b8243a2efe074b16b877b
Matthew-Smith <matthewsmith@cmail.carleton.ca>
2013-02-05 13:32:37 -0500

    First commit with the eclipse project

---
+++ src/pollingSystem/Server.java
@@ -0,0 +1,17 @@
+package pollingSystem;
+
+public class Server {
+
+	public Server() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
