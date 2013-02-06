326b2ebefa245688d9507c6f9f826e820a142dd1
Matthew-Smith <matthewsmith@cmail.carleton.ca>
2013-02-05 23:50:14 -0500

    Server skeleton in place

---
+++ src/pollingSystem/client/AdminClient.java
@@ -0,0 +1,22 @@
+package pollingSystem.client;
+
+import java.io.Serializable;
+
+public class AdminClient implements Serializable {
+
+	/**Generated Serial ID*/
+	private static final long serialVersionUID = -1407412934420712802L;
+
+	public AdminClient() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/pollingSystem/textCase/TestServer.java
+++ src/pollingSystem/textCase/TestServer.java
@@ -4,11 +4,24 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import pollingSystem.server.Server;
+
 public class TestServer {
 
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testCreatePoll() {
+		Server s = new Server();
+		
+		s.startAdminListener();
+		
+		//send message on port 5000
+		
+		assertTrue(s.numberOfPolls()==1);
 	}
 
 }
